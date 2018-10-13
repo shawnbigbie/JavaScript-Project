73c82e5fefc379ddc46ce1ed9dc826af3d7739d2
Shawn Bigbie <boboargos@outlook.com>
2018-10-13 16:27:10 -0600

    tried to add 2nd api

--- js/Main.js
+++ js/Main.js
@@ -1,12 +1,14 @@
 let openingCrawl = document.querySelector("#openingCrawl")
 let movieTitle = document.querySelector("#movieTitle")
+// let name = document.querySelector("#name")
 
-fetch('https://swapi.co/api/films/1')
+// fetch('https://swapi.co/api/people/3')
+fetch('https://swapi.co/api/films/7')
     .then(function(response){
         return response.json();
 })
     .then(function(myJson){
         openingCrawl.textContent = myJson.opening_crawl
         movieTitle.textContent = myJson.title
-        console.log(myJson.opening_crawl);
+//      name.textContent = myJson.name
 })
