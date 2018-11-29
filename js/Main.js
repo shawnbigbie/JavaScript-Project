6d6fdcf9fb537a14c1b65e01c73ed2bbc7e7761d
Shawn <Shawnbigbie@outlook.com>
2018-11-29 12:49:59 -0700

    update 11/29

--- js/Main.js
+++ js/Main.js
@@ -1,109 +1,22 @@
-import { senatorData} from '../assets/senators.js'
-//import { films } from '../assets/films.js'
-//import { people } from '../assets/people.js'
-//import { species } from '../assets/species.js'
+// the function which handles the input field logic
+function getUserAdajective() {
+    var inputField = document.getElementById('inputField').value;
+    var result = document.getElementById('result');
+}
 
-//let openingCrawl = document.querySelector("#openingCrawl")
-//let movieTitle = document.querySelector("#movieTitle")
-//let personName = document.querySelector("#personName")
-
-//let results = [
-//{
-//	"name": "R2-D2",
-//	"height": "96",
-//	"mass": "32",
-//	"hair_color": "n/a",
-//	"skin_color": "white, blue",
-//	"eye_color": "red",
-//	"birth_year": "33BBY",
-//	"gender": "n/a",
-//	"homeworld": "https://swapi.co/api/planets/8/",
-//	"films": [
-//		"https://swapi.co/api/films/2/",
-//		"https://swapi.co/api/films/5/",
-//		"https://swapi.co/api/films/4/",
-//		"https://swapi.co/api/films/6/",
-//		"https://swapi.co/api/films/3/",
-//		"https://swapi.co/api/films/1/",
-//		"https://swapi.co/api/films/7/"
-//	],
-//	"species": [
-//		"https://swapi.co/api/species/2/"
-//	],
-//	"vehicles": [],
-//	"starships": [],
-//	"created": "2014-12-10T15:11:50.376000Z",
-//	"edited": "2014-12-20T21:17:50.311000Z",
-//	"url": "https://swapi.co/api/people/3/"
-//}
-//]
-
-// fetch('https://swapi.co/api/people/3')
-// fetch('https://swapi.co/api/films/7')
-//    .then(function(response){
-//        return response.json();
-//})
-//    .then(function(myJson){
-//        openingCrawl.textContent = myJson.opening_crawl
-//        movieTitle.textContent = myJson.title
-//      name.textContent = myJson.name
-//})
-
-// results.forEach( (AnyName) => {
-// console.log(AnyName)
-// });
-
-//results.forEach( (people) =>{
-//    let listItem =document.createElement('p')
-//    listItem.textContent = people.name
-//    personName.appendChild(listItem)
-//})
-
-//  let listItem =document.createElement('p')
-//  listItem.textContent = results[0].opening_crawl
-//  personName.appendChild(listItem)
+// Store the input field in Arrays
 
 
-// Import and Export JS //
-const senators = senatorData.results[0].members
 
-const republicans = senators.filter(senator => senator.party === 'R')
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', getUserMadLib, false);
 
-const democrats = senators.filter(senator => senator.party === 'D')
 
-const females = senators.filter(senator => senator.gender === 'F')
-const males = senators.filter(senator => senator.gender === 'M')
+// Result OR Print Out Mad Lib
+function getUserMadLib(){
+    var madlib = document.getElementById('madlib');
 
-const loyalRepublican = republicans.reduce((acc, senator) => (senator.votes_with_party_pct > 0) ? senator : acc, 0)
+    const result = (`Today I went to the zoo. I saw a ${adajective[0]}${noun[0]} jumping up and down in its tree. He ${verb[0]}${adverb[0]} through the large tunnel that led to its ${adajective[1]}${noun[1]}. I got some peanuts and passed them through the cage to a gigantic gray ${noun[2]} towering above my head. Feeding that animal made me hungry. I went to get a ${adajective[2]} scoop of ice cream. It filled my stomach. Afterwards I had to ${verb[1]}${adverb[1]} to catch our bus. When I got home I ${verb[2]} my mom for a ${adajective[3]} day at the zoo.`)
 
-console.log(`There are ${republicans.length} republicans and ${democrats.length} democrats in the Senate.`)
-
-console.log(`There are ${males.length} males in the Senate and ${females.length} females`)
-
-console.log(`The most loyal republican is ${loyalRepublican.first_name} ${loyalRepublican.last_name} from ${loyalRepublican.state} who votes with republicans ${loyalRepublican.votes_with_party_pct}% of the time.`)
-
-// makes a new prop for each senator added senator imgURL to each one with there govtrack id
-const senWithPics = senators.map(senator => {
-    senator.imgURL = `https://www.govtrack.us/data/photos/${senator.govtrack_id}-200px.jpeg`
-    if(senator.govtrack_id === '412743'){
-        senator.imgURL = '../assets/220px-Cindy_Hyde-Smith_official_photo.jpeg'
-    }
-    return senator
-})
-
-console.log(senWithPics)
-
-// adding the images to the div with class id of img container
-let pictureDiv = document.querySelector('.imgcontainer')
-
-senWithPics.forEach(senator => {
-    let senatorPic = document.createElement('img')// dose this to each sen
-    // senatorPic.className = "test" adds the html class to be asined to test
-    let senatorFig = document.createElement('figure')
-    let senatorCap = document.createElement('figcaption')
-    senatorCap.textContent = `${senator.first_name} ${senator.last_name}`
-    senatorPic.src = senator.imgURL
-    senatorFig.appendChild(senatorPic)
-    senatorFig.appendChild(senatorCap)
-    pictureDiv.appendChild(senatorFig)
-})
+    madlib.textContent = result;
+}
